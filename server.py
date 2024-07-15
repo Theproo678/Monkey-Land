5b41db2fd24256e89735210762918426a15c8d68
Theproo678 <Alejandrogira@outlook.com>
2024-07-15 20:25:30 +0000

    Mundo guardado 2024-07-15

--- server.py
+++ server.py
@@ -12,16 +12,16 @@
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
-# jp		      Japón (Tokyo)
+# jp		      Japón (Tokyo) 
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
 
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
 
 
 
